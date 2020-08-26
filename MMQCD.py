5175d3b259cbf5a172eda94b98da13d4404b0c34
rgjha <rgjha1989@gmail.com>
2020-08-26 13:41:14 +0530

    Update, working on O6

--- MMQCD.py
+++ MMQCD.py
@@ -13,19 +13,22 @@ dimG = (Nc**2) - 1
 size_col = dimG * 3 
 n_O1_2boson = 24
 n_O2_3boson = 324
-n_04 = 3420
+n_h = 3420
 O1state = np.zeros((n_O1_2boson,3, dimG))
 O1norm = np.zeros((n_O1_2boson))
 O2state = np.zeros((n_O2_3boson, 3, dimG))
 O2norm = np.zeros((n_O2_3boson))
-O32state = np.zeros((n_O2_3boson, 3, dimG))
-O32norm = np.zeros(n_O2_3boson)
-O4state = np.zeros((n_04, 3, dimG))
-O4norm = np.zeros((n_04))
-O5state = np.zeros((n_04, 3, dimG))
-O5norm = np.zeros((n_04))
-matrix_bos = np.zeros((3, dimG))
-
+O3state = np.zeros((n_O2_3boson, 3, dimG))
+O3norm = np.zeros(n_O2_3boson)
+O4state = np.zeros((n_h, 3, dimG))
+O4norm = np.zeros((n_h))
+O5state = np.zeros((n_h, 3, dimG))
+O5norm = np.zeros((n_h))
+O6state = np.zeros((n_h, 3, dimG))
+O6norm = np.zeros((n_h))
+O6totalstate = np.zeros((n_h, 3, dimG))
+O6totalnorm = np.zeros((n_h))
+vac = np.zeros((3, dimG)) # Vacuum state
 
 # Setting up SU(3) structure constants 
 # Note that standard f_123 is denoted as f_012 here.
@@ -70,29 +73,33 @@ def action_A_or_Adag(v, in_vec, in_coeff, choice):
     if tmp >= 0:
       out_coeff = in_coeff * tmp 
     else:
-      print ("Imaginary coefficient! Exit")
-      sys.exit(1)
+      print ("Imaginary coefficient!", tmp, " Exit")
+      #sys.exit(1)
+      return 0 
 
 
   return out_vec, out_coeff
 
 
-def OP1():  # Operator 1 as per Aug 17 notes
+def OP1(in_state, in_state_norm):  # Operator 1 as per Aug 17 notes
 # O1 = A_ij^dagger.A_ij^dagger 
    
   st_num = 0 
+
   for i, a in itertools.product(range(3), range(dimG)): 
 
-      vac = np.zeros((3, dimG)) # Vacuum state
-      O1state[st_num], O1norm[st_num] = action_A_or_Adag((i,a), vac, 1.0, 1)
-      O1state[st_num], O1norm[st_num] = action_A_or_Adag((i,a),O1state[st_num], O1norm[st_num], 1) 
+    O1state[st_num] = in_state
+    O1norm[st_num] = in_state_norm
 
-      st_num += 1 
+    O1state[st_num], O1norm[st_num] = action_A_or_Adag((i,a),O1state[st_num], O1norm[st_num], 1)
+    O1state[st_num], O1norm[st_num] = action_A_or_Adag((i,a),O1state[st_num], O1norm[st_num], 1) 
+
+    st_num += 1 
 
   return O1state, O1norm
 
 
-def OP2():  # Operator 2 
+def OP2(in_state, in_state_norm):  # Operator 2 
 # f_abc ε_ijk (A†_ia * A†_jb * A†_kc) 
   
   st_num = 0 
@@ -106,8 +113,9 @@ def OP2():  # Operator 2
             # This happens for only 324 (=6 x 54) combinations of a,b,c,i,j,k
             # Out of possible 8^3 x 27 = 512 x 27 = 13824  combinations
           
-          vac = np.zeros((3, dimG)) # Vacuum state
-          O2state[st_num], O2norm[st_num] = action_A_or_Adag((k,c), vac, 1.0, 1)
+          O2state[st_num] = in_state
+          O2norm[st_num] = in_state_norm
+          O2state[st_num], O2norm[st_num] = action_A_or_Adag((k,c), O2state[st_num], O2norm[st_num], 1)
           O2state[st_num], O2norm[st_num] = action_A_or_Adag((j,b),O2state[st_num], O2norm[st_num], 1)
           O2state[st_num], O2norm[st_num] = action_A_or_Adag((i,a),O2state[st_num], O2norm[st_num], 1) 
           O2norm[st_num] *= tmp 
@@ -116,7 +124,7 @@ def OP2():  # Operator 2
   return  O2state, O2norm
 
 
-def OP32():  # Operator 3 on 2 on vac
+def OP3(in_state, in_state_norm):  # Operator 3 on 2 on vac
 # f_abc ε_ijk (A_ia * A†_jb * A†_kc)
 # For ex: f_584 ε_321 (A_35 * A†_28 * A†_14)
 # -sqrt(3)/2 (A_35 * A†_28 * A†_14) | O2state >
@@ -129,19 +137,19 @@ def OP32():  # Operator 3 on 2 on vac
         tmp = fabc((a,b,c)) * LeviCivita(i, j, k)
         if tmp != 0:
 
-          O32state[num] = O2state[num]
-          O32norm[num] = O2norm[num]
+          O3state[num] = in_state[num]
+          O3norm[num] = in_state_norm[num]
 
-          O32state[num], O32norm[num] = action_A_or_Adag((k,c),O32state[num], O32norm[num], 1)
-          O32state[num], O32norm[num] = action_A_or_Adag((j,b),O32state[num], O32norm[num], 1)
-          O32state[num], O32norm[num] = action_A_or_Adag((i,a),O32state[num], O32norm[num], 0)
-          O32norm[num] *= tmp
+          O3state[num], O3norm[num] = action_A_or_Adag((k,c),O3state[num], O3norm[num], 1)
+          O3state[num], O3norm[num] = action_A_or_Adag((j,b),O3state[num], O3norm[num], 1)
+          O3state[num], O3norm[num] = action_A_or_Adag((i,a),O3state[num], O3norm[num], 0)
+          O3norm[num] *= tmp
           num += 1
 
-  return  O32state, O32norm
+  return  O3state, O3norm
 
 
-def OP4():  # Operator 4 on vac
+def OP4(in_state, in_state_norm):  # Operator 4 on vac
 # f_abc f_ade (A†_ib * A†_jc * A†_id * A†_je) | vac. >
 
   st_num = 0
@@ -159,8 +167,10 @@ def OP4():  # Operator 4 on vac
               for i in range(3):
                 for j in range(3):
 
-                  vac = np.zeros((3, dimG)) # Vacuum state
-                  O4state[st_num], O4norm[st_num] = action_A_or_Adag((j,e), vac, 1.0, 1)
+                  O4state[st_num] = in_state
+                  O4norm[st_num] = in_state_norm
+
+                  O4state[st_num], O4norm[st_num] = action_A_or_Adag((j,e), O4state[st_num], O4norm[st_num], 1)
                   O4state[st_num], O4norm[st_num] = action_A_or_Adag((i,d),O4state[st_num], O4norm[st_num], 1)
                   O4state[st_num], O4norm[st_num] = action_A_or_Adag((j,c),O4state[st_num], O4norm[st_num], 1)
                   O4state[st_num], O4norm[st_num] = action_A_or_Adag((i,b),O4state[st_num], O4norm[st_num], 1)
@@ -171,8 +181,8 @@ def OP4():  # Operator 4 on vac
   return  O4state, O4norm
 
 
-def OP5():  # Operator 4 on vac
-# f_abc f_ade (A†_ib * A†_jc * A†_id * A†_je) | vac. >
+def OP5(in_state, in_state_norm):  # Operator 4 on vac
+# f_abc f_ade (A_ib * A†_jc * A†_id * A†_je) | vac. >
 
   st_num = 0
 
@@ -189,8 +199,10 @@ def OP5():  # Operator 4 on vac
               for i in range(3):
                 for j in range(3):
 
-                  vac = np.zeros((3, dimG)) # Vacuum state
-                  O5state[st_num], O5norm[st_num] = action_A_or_Adag((j,e), vac, 1.0, 1)
+                  O5state[st_num] = in_state
+                  O5norm[st_num] = in_state_norm
+
+                  O5state[st_num], O5norm[st_num] = action_A_or_Adag((j,e),O5state[st_num], O5norm[st_num], 1)
                   O5state[st_num], O5norm[st_num] = action_A_or_Adag((i,d),O5state[st_num], O5norm[st_num], 1)
                   O5state[st_num], O5norm[st_num] = action_A_or_Adag((j,c),O5state[st_num], O5norm[st_num], 1)
                   O5state[st_num], O5norm[st_num] = action_A_or_Adag((i,b),O5state[st_num], O5norm[st_num], 0)
@@ -201,20 +213,96 @@ def OP5():  # Operator 4 on vac
   return  O5state, O5norm
 
 
+def OP6(in_state, in_state_norm):  
+# f_abc f_ade [(A_ib * A_jc * A†_id * A†_je) + (A_ib * A_id * A†_jc * A†_je) +  \ 
+# (A_ib * A_je * A†_id * A†_jc)] | vac. >
+
+  st_num = 0
+
+  for a in range(dimG):
+    for b in range(dimG):
+      for c in range(dimG):
+        for d in range(dimG):
+          for e in range(dimG):
+
+            tmp = fabc((a,b,c)) * fabc((a,d,e))
+            if tmp != 0:
+
+
+              for i in range(3):
+                for j in range(3):
+
+
+                  O6state[st_num] = in_state[st_num]
+                  O6norm[st_num] = in_state_norm[st_num]
+
+                  print ("with coeff . ", O6norm[st_num])
+                  
+                  O6state[st_num], O6norm[st_num] = action_A_or_Adag((j,c), O6state[st_num], O6norm[st_num], 1)
+                  print ("with coeff .. ", O6norm[st_num])
+                  O6state[st_num], O6norm[st_num] = action_A_or_Adag((i,d),O6state[st_num], O6norm[st_num], 1)
+                  print ("with coeff ... ", O6norm[st_num])
+                  O6state[st_num], O6norm[st_num] = action_A_or_Adag((j,e),O6state[st_num], O6norm[st_num], 0)
+                  print ("with coeff .... ", O6norm[st_num])
+                  O6state[st_num], O6norm[st_num] = action_A_or_Adag((i,b),O6state[st_num], O6norm[st_num], 0)
+                  O6totalstate[st_num] = O6state[st_num]
+                  O6totalnorm[st_num] = O6norm[st_num]
+
+                  print (O6state[st_num].reshape(24), "with coeff", O6norm[st_num])
+
+
+                  O6state[st_num] = in_state[st_num]
+                  O6norm[st_num] = in_state_norm[st_num]
+
+                  O6state[st_num], O6norm[st_num] = action_A_or_Adag((j,e), O6state[st_num], O6norm[st_num], 1)
+                  O6state[st_num], O6norm[st_num] = action_A_or_Adag((j,c),O6state[st_num], O6norm[st_num], 1)
+                  O6state[st_num], O6norm[st_num] = action_A_or_Adag((i,d),O6state[st_num], O6norm[st_num], 0)
+                  O6state[st_num], O6norm[st_num] = action_A_or_Adag((i,b),O6state[st_num], O6norm[st_num], 0)
+
+                  O6totalstate[st_num] += O6state[st_num]
+                  O6totalnorm[st_num] += O6norm[st_num]
+
+                  print (O6state[st_num].reshape(24), "with coeff", O6norm[st_num])
+
+                  O6state[st_num] = in_state[st_num]
+                  O6norm[st_num] = in_state_norm[st_num]
+
+                  O6state[st_num], O6norm[st_num] = action_A_or_Adag((j,e), O6state[st_num], O6norm[st_num], 1)
+                  O6state[st_num], O6norm[st_num] = action_A_or_Adag((i,d),O6state[st_num], O6norm[st_num], 1)
+                  O6state[st_num], O6norm[st_num] = action_A_or_Adag((j,c),O6state[st_num], O6norm[st_num], 0)
+                  O6state[st_num], O6norm[st_num] = action_A_or_Adag((i,b),O6state[st_num], O6norm[st_num], 0)
+
+                  print (O6state[st_num].reshape(24), "with coeff", O6norm[st_num])
+
+                  O6totalstate[st_num] += O6state[st_num]
+                  O6totalnorm[st_num] += O6norm[st_num]
+
+                  O6totalnorm[st_num] *= tmp
+                  st_num += 1
+
+                  sys.exit(1)
+
+
+  return  O6totalstate, O6totalnorm
+
+
 if __name__ == "__main__":
 
-  OP1()
-  OP2()
-  OP32()
-  OP4() 
-  OP5() 
+
+  OP1(vac, 1.0) 
+  OP2(vac, 1.0)
+  OP3(O2state, O2norm)
+  OP4(vac, 1.0) 
+  OP5(vac, 1.0) 
+  OP6(O4state, O4norm) 
 
   for i in range (10): # Print first 10 states of each type for now. 
-    print ("O1:", O1state[i].reshape(size_col), "with coefficient", O1norm[i]) # 24 states #2-boson state 
-    print ("O2:", O2state[i].reshape(size_col), "with coefficient", O2norm[i]) # 324 states  #3-boson state 
-    print ("O32:",O32state[i].reshape(size_col), "with coefficient", O32norm[i]) # 324 states  #5-boson state 
-    print ("O4:",O4state[i].reshape(size_col), "with coefficient", O4norm[i]) # 3420 states # 4-boson state 
-    print ("O5:",O5state[i].reshape(size_col), "with coefficient", O5norm[i]) # 3420 states # 2-boson state 
+    print ("O1 |vac> :", O1state[i].reshape(size_col), "with coefficient", round(O1norm[i],3)) # 24 states #2-boson state 
+    print ("O2 |vac> :", O2state[i].reshape(size_col), "with coefficient", round(O2norm[i],3)) # 324 states  #3-boson state 
+    print ("O3 O2 |vac> :",O3state[i].reshape(size_col), "with coefficient", round(O3norm[i],3)) # 324 states  #5-boson state 
+    print ("O4 |vac> :",O4state[i].reshape(size_col), "with coefficient", round(O4norm[i],3)) # 3420 states # 4-boson state 
+    print ("O5 |vac> :",O5state[i].reshape(size_col), "with coefficient", round(O5norm[i],3)) # 3420 states # 2-boson state 
+    print ("O6 O4 |vac> :",O6totalstate[i].reshape(size_col), "with coefficient", round(O6totalnorm[i],3)) # 3420 states
 
 
 
