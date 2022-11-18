033629dc91a59c50d8e56a4c307d4ec5e49f5a50
Raghav G. Jha <rgjha@users.noreply.github.com>
2022-11-17 22:31:22 -0500

    Update o3ED.py

--- o3ED.py
+++ o3ED.py
@@ -3,11 +3,14 @@ import math
 import sys
 from numpy import linalg as LA
 from numpy.linalg import eig
+from numpy.linalg import svd
 from sympy import Matrix, init_printing
 init_printing()
 
 N=4 
 SQRT2 = 1.4142135623730951
+dimH=8
+padup=int(dimH-2)
 
 if len(sys.argv) < 3:
   print("Usage: python", str(sys.argv[0]), "l-max selected " "beta=1/g^2")
@@ -19,26 +22,28 @@ sigmax = np.array([[0, 1],[1, 0]])
 isigmay =  np.array([[0, 1],[-1, 0]])
 sigmaz =  np.array([[1, 0],[0, -1]])
 
+nplus = -(sigmax + isigmay)/(3*SQRT2)
+nminus = -(sigmax - isigmay)/(3*SQRT2)
 
 if lmax == 0.5:
 
    I2 = np.identity(2)
    I4 = np.identity(4)
-   nplus = -(sigmax + isigmay)/(3*SQRT2)
-   nminus = -(sigmax - isigmay)/(3*SQRT2)
    Ikin = np.identity(16)
    # beta=10 gives -2.18472
 
-if lmax == 1.0:
+if lmax == 1.5:
 
-   Ikin = np.identity(625)
-   sigmaz = np.pad(sigmaz, (0,3), 'constant', constant_values=(0)) 
-   isigmay = np.pad(isigmay, (0,3), 'constant', constant_values=(0)) 
-   sigmax = np.pad(sigmax, (0,3), 'constant', constant_values=(0)) 
-   I2 = np.identity(5)
-   I4 = np.identity(25)
-   nplus = -(1/3)*(1/math.sqrt(2))*(sigmax + isigmay)
-   nminus = -(1/3)*(1/math.sqrt(2))*(sigmax - isigmay)
+   Ikin = np.identity(dimH**4)
+   sigmaz = np.pad(sigmaz, (0,padup), 'constant', constant_values=(0)) 
+   isigmay = np.pad(isigmay, (0,padup), 'constant', constant_values=(0)) 
+   sigmax = np.pad(sigmax, (0,padup), 'constant', constant_values=(0)) 
+   I2 = np.identity(dimH)
+   I4 = np.identity(dimH**2)
+   nplus = np.kron(np.kron(nplus, nplus), nplus) 
+   nminus = np.kron(np.kron(nminus, nminus), nminus) 
+   #nplus = np.pad(nplus, (0,padup), 'constant', constant_values=(0)) 
+   #nminus = np.pad(nminus, (0,padup), 'constant', constant_values=(0)) 
 
 
 HH = (N*(3./8.)*(1/beta)*Ikin) + \
@@ -48,9 +53,12 @@ beta*(np.kron(np.kron(I4, nplus),nminus) + np.kron(np.kron(I4, nminus),nplus) + 
 beta*(np.kron(np.kron(nminus, I4),nplus) + np.kron(np.kron(nplus, I4),nminus) + (1./9.0)*np.kron(np.kron(sigmaz, I4), sigmaz)) 
 
 
+# Try padding H rather than what makes H 
+#HH1= np.pad(HH, (0,4080), 'constant', constant_values=(0))
+
 #with np.printoptions(precision=10, suppress=True, formatter={'float': '{:0.2f}'.format}, linewidth=100):
 #    print(HH)
 
-print (np.shape(HH))
-w, v = eig(HH)
+print (np.shape(HH1))
+w, v = eig(HH1)
 print ("ground state energy", np.min(w)/N)
